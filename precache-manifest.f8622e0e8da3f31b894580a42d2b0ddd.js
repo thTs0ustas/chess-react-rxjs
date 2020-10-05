e5c78cfcdcae197e049b7f8a71f4f7cebab0381c
thtsoustas <th.ts0ustas@gmail.com>
2020-10-05 19:43:44 +0300

    Updates

---
+++ precache-manifest.f8622e0e8da3f31b894580a42d2b0ddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf750ddd81250d16d361f357516532c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78893ca44f8b0aa3592e",
+    "url": "/static/css/main.0edcdbb9.chunk.css"
+  },
+  {
+    "revision": "c2f98362197f59255895",
+    "url": "/static/js/2.ddfa4509.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.ddfa4509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78893ca44f8b0aa3592e",
+    "url": "/static/js/main.e9f10adc.chunk.js"
+  },
+  {
+    "revision": "e4db7c41fb030e5ad0ee",
+    "url": "/static/js/runtime-main.ebb47c36.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/static/media/q_w.3382a953.png"
+  }
+]);
